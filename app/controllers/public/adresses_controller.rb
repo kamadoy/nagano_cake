1ff21cd7bcbd62ef805cbdb841b99dab24f3ef5f
EC2 Default User <ec2-user@ip-172-31-31-154.ec2.internal>
2022-02-27 08:13:20 +0000

    AddAdress

--- app/controllers/public/adresses_controller.rb
+++ app/controllers/public/adresses_controller.rb
@@ -1,7 +1,35 @@
 class Public::AdressesController < ApplicationController
   def index
+    @adress = Adress.new
+    @adresses = Adress.all
   end
 
-  def deit
+  def edit
+    @adress = Adress.find(params[:id])
+  end
+
+  def update
+     @adress = Adress.find(params[:id])
+     @adress.update(adress_params)
+     redirect_to adresses_path
+  end
+
+  def create
+    @adress = Adress.new(adress_params)
+    @adress.save
+
+    redirect_to adresses_path
+  end
+
+  def destroy
+   @adress = Adress.find(params[:id])
+   @adress.destroy
+   redirect_to adresses_path
+  end
+
+   private
+
+  def adress_params
+    params.require(:adress).permit(:customer_id, :name,:postal_code,:adress,:create_at,:update_at)
   end
 end
--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -19,6 +19,8 @@ class Public::CustomersController < ApplicationController
   def withdraw
   end
 
+private
+
   def customer_params
     params.require(:customer).permit(:last_name, :first_name,:last_name_kana, :first_name_kana,:emai,:encrypted_password,:postal_code,:adress,:telephone_number,:is_active,:create_at,:update_at)
   end
